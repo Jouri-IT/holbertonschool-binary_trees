2c141996a803090faf3053e9531314f1f640b4e4
Jouri-IT <jouri.f.alsulaiman@gmail.com>
2026-04-28 17:12:33 +0300

    Add binary_tree_postorder prototype to header

--- binary_trees.h
+++ binary_trees.h
@@ -1,8 +1,8 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -22,8 +22,7 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
-/* Prototypes */
-void binary_tree_print(const binary_tree_t *tree);
+/* Task prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -32,5 +31,9 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Provided print function */
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
